5a7ad8c38781ce9ceab83e66ad8a283d5e70df4c
devico <clamdm@gmail.com>
2016-07-31 12:26:16 +0300

    movie_spec - не работает

--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -6,16 +6,14 @@ require "date"
 
 describe '#self.create' do
 
-  let(:movies) { MovieCollection.new("movies.txt") }
-  let(:modern_movie) { ModernMovie.new("http://imdb.com/title/tt0111161/?ref_=chttp_tt_1",
-    "The Shawshank Redemption", "1994", "USA", "1994-10-14", "Crime,Drama", "142 min", "9.3",
-    "Frank Darabont", "Tim Robbins,Morgan Freeman,Bob Gunton", movies) }
+#  let(:netflix) { Netflix.new("movies.txt") }
+  let(:year) { 1994 }
+  puts year
+  let(:period) { ModernMovie if year.include?(1968..2000) }
+  let(:movie_class) { period.new("http://imdb.com/title/tt0111161/?ref_=chttp_tt_1", "The Shawshank Redemption", "1994", "USA", "1994-10-14", "Crime,Drama", "142 min", "9.3", "Frank Darabont", "Tim Robbins,Morgan Freeman,Bob Gunton", movies).class }
 
-  subject { Movie.create(*params) }
-
-  context 'verify movie class' do
-    let(:params) {[]}
-    it { expect( subject ).to eq(modern_movie.class) }
+  context 'movie_class' do
+    it { is_expected.to be_a ModernMovie }
   end
 
 
