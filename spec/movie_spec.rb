fa09bce9a3980b51bd61b34905ce0273533ede09
devico <clamdm@gmail.com>
2016-07-31 13:33:19 +0300

    movie_spec - заработал

--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -2,17 +2,17 @@ require "rspec"
 require_relative "../movie"
 require_relative "../modern_movie"
 require_relative "../movie_collection"
+require_relative "../netflix"
 require "date"
 
 describe '#self.create' do
-
-#  let(:netflix) { Netflix.new("movies.txt") }
   let(:year) { 1994 }
-  puts year
-  let(:period) { ModernMovie if year.include?(1968..2000) }
-  let(:movie_class) { period.new("http://imdb.com/title/tt0111161/?ref_=chttp_tt_1", "The Shawshank Redemption", "1994", "USA", "1994-10-14", "Crime,Drama", "142 min", "9.3", "Frank Darabont", "Tim Robbins,Morgan Freeman,Bob Gunton", movies).class }
+  let(:period) { ModernMovie if (1968..2000).include?(year) }
+  let(:movie_class) {period.new("http://imdb.com/title/tt0111161/?ref_=chttp_tt_1", "The Shawshank Redemption", "1994", "USA", "1994-10-14", "Crime,Drama", "142 min", "9.3", "Frank Darabont", "Tim Robbins,Morgan Freeman,Bob Gunton", self) }
 
-  context 'movie_class' do
+  subject { movie_class }
+
+  context 'when movie has class ModernMovie' do
     it { is_expected.to be_a ModernMovie }
   end
 
