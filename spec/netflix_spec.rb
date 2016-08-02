4a4f3700cdb02cd04131f4b1cb1dc6d10acf28e8
devico <clamdm@gmail.com>
2016-08-02 09:35:39 +0300

    тест netflix#show

--- spec/netflix_spec.rb
+++ spec/netflix_spec.rb
@@ -9,29 +9,15 @@ require_relative "../netflix"
 require "date"
 
 
-# describe '#show' do
+describe '#show' do
 
-#   let(:netflix) { Netflix.new("movies.txt") }
-#   let(:movie) { netflix.all.first }
-#   let(:value) { "#{movie.title} — современное кино: играют #{movie.actors.join(", ")}" }
+  subject { netflix.show(params) }
 
-#   subject { netflix.show(params) }
-
-#   context 'send parameters' do
-#     let(:params){ [genre: 'Comedy', period: :modern] }
-#     it { expect( subject ).to eq(value) }
-#   end
-# end
-
-describe '#pay' do
-
-  let(:netflix) { Netflix.new("movies.txt") }
-  let(:params) { 25 }
-
-  subject { netflix.pay(params) }
-
-  context 'when send pay, balance equal 25' do
-    it { expect( subject ).to eq 25 }
-  end
-
-end
+    context 'send parameters' do
+      let(:netflix) { Netflix.new("movies.txt") }
+      let(:movie) { netflix.all.first }
+      let(:value) { "#{movie.title} — современное кино: играют #{movie.actors.join(", ")}" }
+      let(:params){ {genre: 'Crime', period: :modern} }
+      it { expect( subject ).to eq(value) }
+   end
+ end
