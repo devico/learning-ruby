daee6218c51d1e42382aad16c224703b7a4a7316
devico <clamdm@gmail.com>
2016-08-03 21:04:43 +0300

    тест #show работает

--- spec/netflix_spec.rb
+++ spec/netflix_spec.rb
@@ -9,22 +9,6 @@ require_relative "../movie_collection"
 require_relative "../netflix"
 require "date"
 
-
-# describe '#show' do
-
-#   subject { netflix.show(params) }
-
-#     context 'send parameters' do
-#       let(:netflix) { Netflix.new("movies.txt") }
-#       let(:movie) { netflix.filter(genre: 'Comedy').first }
-#       let(:start_time) { Time.now }
-#       let(:end_time) { start_time + movie.length.to_i * 60 }
-#       let(:value) { "#{movie.title} — современное кино: играют #{movie.actors.join(", ")}" }
-#       let(:params){ {genre: 'Comedy', period: :modern} }
-#       it { expect( subject ).to eq(value) }
-#    end
-# end
-
 describe '#pay' do
 
   subject { netflix.pay(params) }
@@ -39,27 +23,14 @@ describe '#pay' do
 
 describe '#show with payment' do
 
-subject { netflix }
-before { subject.pay(initial_balance) }
-context 'after movie is shown' do
-  before { subject.show(movie) }
-  let(:netflix) { Netflix.new("movies.txt") }
-
-  its(:balance) { is_expected.to eq(initial_balance - movie.cost) }
+  subject { netflix }
+  before { subject.pay(initial_balance) }
+  context 'after movie is shown' do
+    before { subject.show(params) }
+    let(:netflix) { Netflix.new("movies.txt") }
+    let(:initial_balance) { 25 }
+    let(:movie) { netflix.filter(genre: 'Comedy').first }
+    let(:params) { {genre: 'Comedy', period: :modern} }
+    its(:balance) { is_expected.to eq(initial_balance - movie.cost) }
+  end
 end
-end
-
- # describe '#show with payment' do
-
- #  subject { netflix.show(params) }
-
- #    context 'when show movie balance decrease' do
- #      let(:netflix) { Netflix.new("movies.txt") }
- #      let(:balance) { netflix.pay(25).to_f }
- #      let(:movie) { netflix.filter(genre: 'Comedy').first }
- #      let(:cost)  {movie.cost}
- #      let(:value) { balance - cost }
- #      let(:params){ {genre: 'Comedy', period: :modern} }
- #      it { expect( subject ).to eq(value) }
- #   end
- # end
