9b2762d37840d3eddc0acff68ab44a3b15106bc6
devico <clamdm@gmail.com>
2016-08-04 11:08:04 +0300

    netflix_spec.rb переделан с учетом общего before

--- spec/netflix_spec.rb
+++ spec/netflix_spec.rb
@@ -9,41 +9,41 @@ require_relative "../movie_collection"
 require_relative "../netflix"
 require "date"
 
-describe '#pay' do
+describe Netflix do
 
-  subject { netflix.pay(params) }
+  before { @netflix = Netflix.new("movies.txt") }
 
-    context 'when pay 25, balance increase to 25 ' do
-      let(:netflix) { Netflix.new("movies.txt") }
+  describe 'Netflix#pay' do
+    subject { @netflix.pay(params) }
+    context '#pay when pay 25, balance increase to 25 ' do
+#      let(:netflix) { Netflix.new("movies.txt") }
       let(:params){ 25 }
       let(:balance) { 25 }
       it { expect( subject ).to eq(balance) }
-   end
- end
-
-describe '#show with payment' do
-
-  subject { netflix }
-  before { subject.pay(initial_balance) }
-  context 'after movie is shown' do
-    before { subject.show(params) }
-    let(:netflix) { Netflix.new("movies.txt") }
-    let(:initial_balance) { 25 }
-    let(:movie) { netflix.filter(genre: 'Comedy').first }
-    let(:params) { {genre: 'Comedy', period: :modern} }
-    its(:balance) { is_expected.to eq(initial_balance - movie.cost) }
+    end
   end
-end
 
-describe '#how_much?' do
+  describe 'Netflix#show' do
+    subject { @netflix }
+    before { subject.pay(initial_balance) }
+    context '#show with pay after movie is shown' do
+      before { subject.show(params) }
+#      let(:netflix) { Netflix.new("movies.txt") }
+      let(:initial_balance) { 25 }
+      let(:movie) { @netflix.filter(genre: 'Comedy').first }
+      let(:params) { {genre: 'Comedy', period: :modern} }
+      its(:balance) { is_expected.to eq(initial_balance - movie.cost) }
+    end
+  end
 
-  subject { netflix.how_much?(params) }
-
-  context "when asked how much movie" do
-    let(:netflix) { Netflix.new("movies.txt") }
-    let(:movie) { netflix.filter(title: 'The Terminator').first }
-    let(:params) { {title: 'The Terminator'} }
-    it { expect( subject ).to eq(movie.cost) }
+  describe 'Netflix#film_costs' do
+    subject { @netflix.film_costs(params) }
+    context "#film_costs - when asked how much movie" do
+#      let(:netflix) { Netflix.new("movies.txt") }
+      let(:movie) { @netflix.filter(title: 'The Terminator').first }
+      let(:params) { {title: 'The Terminator'} }
+      it { expect( subject ).to eq(movie.cost) }
+    end
   end
 
 end
