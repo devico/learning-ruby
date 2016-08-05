ad5c4a99c4e06acc791463fa289c775a40e54055
devico <clamdm@gmail.com>
2016-08-05 18:06:29 +0300

    код и тесты для обычного кинотеатра

--- spec/theatre_spec.rb
+++ spec/theatre_spec.rb
@@ -1,22 +1,33 @@
 require "rspec"
+require 'spec_helper'
 require_relative "../movie"
+require_relative "../ancient_movie"
+require_relative "../classic_movie"
+require_relative "../modern_movie"
+require_relative "../new_movie"
 require_relative "../movie_collection"
 require_relative "../theatre"
 require "date"
 
-describe '#show' do
+describe Theatre do
 
-  let(:movies) { MovieCollection.new("movies.txt") }
-  let(:movie) { movies.first }
-  let(:theatre) { Theatre.new }
-  let(:start_time) { Time.now }
-  let(:end_time) { start_time + movie.length.to_i * 60 }
-  let(:value) { "Now showing: #{movie.title} #{start_time.strftime("%H:%M")} - #{end_time.strftime("%H:%M")}"}
+  let(:theatre) { Theatre.new("movies.txt")}
 
-  subject { theatre.show(*params) }
-
-  context 'send parametrs' do
-      let(:params) { [movie, start_time, end_time] }
-      it { expect( subject ).to eq value }
+  describe "#time_of_day?" do
+    subject { theatre.time_of_day?(params) }
+    context 'send parametrs' do
+      let(:value) { :afternoon }
+      let(:params) { '15:20' }
+      it { expect( subject ).to eq(value) }
+    end
   end
+
+  describe "when show movie" do
+    subject{ theatre.show(params)}
+    context "show movie for this time" do
+      let(:params) { '10:20' }
+      it { is_expected.to be_a AncientMovie }
+    end
+  end
+
 end
--- theatre.rb
+++ theatre.rb
@@ -1,11 +1,30 @@
+require 'date'
+
 class Theatre < MovieCollection
 
-  def initialize
-
+  def time_of_day?(time)
+   order_hour = time.split(':')[0]
+   time_of_day = case order_hour.to_i
+      when 8..12 then :morning
+      when 13..16 then :afternoon
+      when 17..23 then :evening
+      else :night
+    end
   end
 
-  def show(movie, start_time, end_time)
-    "Now showing: #{movie.title} #{start_time.strftime("%H:%M")} - #{end_time.strftime("%H:%M")}"
+  def show(params)
+
+   order_time = self.time_of_day?(params)
+
+   order_movie = case order_time
+    when :morning then {period: :ancient}
+    when :afternoon then {genre: 'Comedy', genre: 'Adventure'}
+    when :evening then {genre: 'Drama', genre: 'Horrors'}
+    else "В ночное время кинотеатр не работает"
+    end
+
+   movie = self.filter(order_movie).first
+
   end
 
 end
