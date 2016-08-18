adfd0b7742c0acab5ebefdeb56e05dcd3e6c9f57
devico <clamdm@gmail.com>
2016-08-18 23:04:49 +0300

    тест ancient_movie_spec не проходит

--- spec/ancient_movie_spec.rb
+++ spec/ancient_movie_spec.rb
@@ -1,20 +1,24 @@
-describe AncientMovie do
+module TopMovies
 
-  let(:netflix) { Netflix.new("movies.txt") }
-  let(:movie) { netflix.filter(period: :ancient).first }
+  describe AncientMovie do
 
-  describe '#cost and #period' do
-    subject { movie }
-    its(:cost){ is_expected.to eq(1.0) }
-    its(:period){ is_expected.to eq(:ancient) }
-  end
+    let(:netflix) { Netflix.new("movies.txt") }
+    let(:movie) { netflix.filter(period: :ancient).first }
 
-  describe '#show' do
-    subject { movie.show }
-    context 'when show movie puts in format: Title — старый фильм (год)' do
-      let(:value) { "#{movie.title} — старый фильм (#{movie.year} год)" }
-      it { expect( subject ).to eq(value) }
+    describe '#cost and #period' do
+      subject { movie }
+      its(:cost){ is_expected.to eq(1.0) }
+      its(:period){ is_expected.to eq(:ancient) }
     end
+
+    describe '#show' do
+      subject { movie.show }
+      context 'when show movie puts in format: Title — старый фильм (год)' do
+        let(:value) { "#{movie.title} — старый фильм (#{movie.year} год)" }
+        it { expect( subject ).to eq(value) }
+      end
+    end
+
   end
 
 end
