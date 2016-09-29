cd2a57aa0d7f7b8eee560c7338e2980fd2fc2257
devico <clamdm@gmail.com>
2016-09-29 13:25:21 +0300

    imdb_budgets.rb

--- imdb_budgets.rb
+++ imdb_budgets.rb
@@ -3,26 +3,48 @@ require 'open-uri'
 require 'yaml'
 require 'ruby-progressbar'
 module ImdbBudgets
+
   def take_budget
     file_name = 'budget.yml'
     if File.exist?(file_name)
       YAML.load_file(File.read(file_name)).each { |item| puts item }
     else
-      progressbar = ProgressBar.create
-      progressbar.total = @collection.size
       budgetfile = File.open(file_name, 'w')
-      create_yml(budgetfile, progressbar)
+      create_yml(budgetfile)
       budgetfile.close
     end
   end
 
-  def create_yml(budgetfile, progressbar)
-    @collection.map do |film|
-      page = Nokogiri::HTML(open(film.link))
-      bd = page.css('div.txt-block:nth-child(11)').map { |el| el.text.split(' ')[1] }
-      data = { 'title' => film.title, 'budget' => bd[0] }.to_yaml
-      budgetfile.puts data
+  def create_yml(budgetfile)
+    pages = obtain_pages
+    movie_info = take_info(pages)
+    data = info_to_yml(movie_info)
+    put_to_file(budgetfile, data)
+  end
+
+  def obtain_pages
+    progressbar = ProgressBar.create
+    progressbar.total = @collection.size
+    pages = @collection.map do |film|
       progressbar.increment
+      Nokogiri::HTML(open(film.link))
     end
   end
+
+  def take_info(pages)
+    info = pages.map do |mp|
+      imdb_id = mp.at("meta[property='pageId']")['content']
+      budget = mp.css('div.txt-block:nth-child(11)').map { |el| el.text.split(' ')[1] }
+      info = [imdb_id, budget[0]]
+    end
+  end
+
+  def info_to_yml(movie_info)
+    budgets = movie_info.map { |i| { 'imdb_id' => i[0], 'budget' => i[1] }.to_yaml if i[1] =~ /^(\$|\€)/ }.compact
+  end
+
+  def put_to_file(budgetfile, data)
+    budgetfile.puts data
+  end
+
 end
--- spec/imdb_budgets_spec.rb
+++ spec/imdb_budgets_spec.rb
@@ -2,16 +2,26 @@ module TopMovies
 
   describe ImdbBudgets do
 
-    describe '#take_budget' do
-      it "given a budget" do
-        VCR.use_cassette('imdb/budget') do
-          stub_request(:get, "http://imdb.com/title/tt0103064/?ref_=chttp_tt_41").
+    describe '#obtain_pages' do
+      it "given a pages" do
+        VCR.use_cassette('imdb/pages') do
+          stub_request(:get, "http://imdb.com/title/tt0111161/?ref_=chttp_tt_41").
             with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
             to_return(:status => 200, :body => "", :headers => {})
-          page = Nokogiri::HTML(open("http://imdb.com/title/tt0103064/?ref_=chttp_tt_41"))
+          page = Nokogiri::HTML(open("http://imdb.com/title/tt0111161/?ref_=chttp_tt_41"))
           expect(page.class).to eq(Nokogiri::HTML::Document)
+          expect(page.css('div.txt-block:nth-child(11)').map { |el| el.text.split(' ')[1] }).to be_a Array
         end
       end
     end
+
+    describe '#take_info' do
+
+    end
+
+    describe '#info_to_yml' do
+
+    end
+
   end
 end
