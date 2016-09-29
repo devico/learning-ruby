10c9318084b5d17faaf6f3a9a3710ff6ec44e862
devico <clamdm@gmail.com>
2016-09-29 20:19:01 +0300

    сделал тесты imdb_budgets_spec.rb

--- spec/imdb_budgets_spec.rb
+++ spec/imdb_budgets_spec.rb
@@ -2,6 +2,12 @@ module TopMovies
 
   describe ImdbBudgets do
 
+    let(:movies) { TopMovies::MovieCollection.new("movies.txt") { include ImdbBudgets } }
+
+    describe '#take_budget' do
+
+    end
+
     describe '#obtain_pages' do
       it "given a pages" do
         VCR.use_cassette('imdb/pages') do
@@ -16,11 +22,24 @@ module TopMovies
     end
 
     describe '#take_info' do
-
+      it 'when recieve info about movie' do
+        stub_request(:get, "http://imdb.com/title/tt0111161/?ref_=chttp_tt_41").
+         with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
+         to_return(:status => 200, :body => "", :headers => {})
+        page = Nokogiri::HTML(open("http://imdb.com/title/tt0111161/?ref_=chttp_tt_41"))
+        expect(movies.take_info(page)).to be_a Array
+      end
     end
 
     describe '#info_to_yml' do
-
+      it 'when save info to file' do
+        stub_request(:get, "http://imdb.com/title/tt0111161/?ref_=chttp_tt_41").
+         with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
+         to_return(:status => 200, :body => "", :headers => {})
+        page = Nokogiri::HTML(open("http://imdb.com/title/tt0111161/?ref_=chttp_tt_41"))
+        info = movies.take_info(page)
+        expect(movies.info_to_yml(info)).to be_a Array
+      end
     end
 
   end
--- tmdb_posters.rb
+++ tmdb_posters.rb
@@ -5,7 +5,7 @@ module TmdbPosters
       imdb_id = film.link.scan(/tt\d{7}/).first
       id_mov = Tmdb::Find.movie(imdb_id, external_source: 'imdb_id').first.id
       Tmdb::Movie.posters(id_mov).first
-      # Tmdb::Movie.translations(id_mov)
+      Tmdb::Movie.translations(id_mov)
     end
   end
 end
